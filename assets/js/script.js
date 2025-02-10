d7a57d677c0bb7fd8fe3812e084e45b90cb6cc01
irnano <valdepenas.adrian@gmail.com>
2025-02-10 22:35:38 +0800

    removed navbar bootstrap

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,26 @@
-$('#exampleModal').on('show.bs.modal', function (event) {
-  var button = $(event.relatedTarget) // Button that triggered the modal
-  var recipient = button.data('src') 
-  var title = button.data('title')
-  var price = button.data('price')
-  $('.alert').hide()
+// NAVBAR BURGER BUTTON
+const toggleButton = document.querySelector(".navbar-toggler");
+const collapseContainer = document.querySelector(".collapse");
+
+toggleButton.addEventListener("click", () => {
+  collapseContainer.classList.toggle("show");
+});
+
+// MODAL
+
+$("#exampleModal").on("show.bs.modal", function (event) {
+  var button = $(event.relatedTarget); // Button that triggered the modal
+  var recipient = button.data("src");
+  var title = button.data("title");
+  var price = button.data("price");
+  $(".alert").hide();
   // Extract info from data-* attributes
   // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
   // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
-  
-  var modal = $(this)
-  modal.find('.modal-title').text(title)
-  modal.find('.modal-price').text(price)
+
+  var modal = $(this);
+  modal.find(".modal-title").text(title);
+  modal.find(".modal-price").text(price);
   // modal.find('[src=""]').val(recipient)
   // $('[src]').val(recipient)
   // console.log('test:'+modal.find('img').attr());
@@ -18,16 +28,14 @@ $('#exampleModal').on('show.bs.modal', function (event) {
   // const products = document.querySelector('.card-img-top');
   // console.log('product:'+products.dataset.src);
 
-  var img = $('#modal-img')
-  img.attr('src', recipient);
+  var img = $("#modal-img");
+  img.attr("src", recipient);
+});
 
-})
+$("#send-button").click(function () {
+  $(".alert").show();
+});
 
-$('#send-button').click(function(){
-  $('.alert').show();
-})
-
-$('.alert').on('closed.bs.alert', function(){
-  $('alert').show();
-})
-
+$(".alert").on("closed.bs.alert", function () {
+  $("alert").show();
+});
